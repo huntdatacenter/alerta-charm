d620db6e73a7e7711d79aab2cb7562e62a0e36de
Matus Kosut <matus.kosut@ntnu.no>
2020-01-15 22:18:14 +0100

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Alerta Charm
+
+Alerta juju charm provides Alerta service with web UI.
+
+# Overview
+
+Alerta accepts alerts from the standard sources like Syslog, SNMP, Prometheus, Nagios, Zabbix, Sensu and netdata. Any monitoring tool that can trigger a URL request can be integrated easily. Anything that can be scripted can also send alerts using the command-line tool. There is already a Python SDK and other SDKs are in the pipeline.
+
+## Links
+
+- https://alerta.io/
+- https://docs.alerta.io/en/latest/gettingstarted/tutorial-1-deploy-alerta.html#step-3-customisation
---
+++ reactive/reactive.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+from charmhelpers.contrib.ansible import apply_playbook
+from charmhelpers.core import hookenv
+from charmhelpers.core.hookenv import application_version_set
+from charmhelpers.core.hookenv import log
+from charmhelpers.core.hookenv import open_port
+from charmhelpers.core.hookenv import status_set
+from charms.reactive import hook
+from charms.reactive import remove_state
+from charms.reactive import set_state
+from charms.reactive import when
+from charms.reactive import when_not
+
+config = hookenv.config()
+
+
+@when_not('alerta.version')
+def set_version():
+    try:
+        with open(file='repo-info') as f:
+            for line in f:
+                if line.startswith('commit-short'):
+                    commit_short = line.split(':')[-1].strip()
+                    application_version_set(commit_short)
+    except IOError:
+        log('Cannot set application version. Missing repo-info.')
+    set_state('alerta.version')
+
+
+@when_not('alerta.installed')
+def install_deps():
+    status_set('maintenance', 'installing dependencies')
+    apply_playbook(
+        playbook='ansible/playbook.yaml',
+        extra_vars={
+            'service_port': config.get('port')
+        }
+    )
+    status_set('active', 'ready')
+    set_state('alerta.installed')
+
+
+@when('website.available')
+def configure_website(website):
+    open_port(config.get('port'))
+    website.configure(port=config.get('port'))
+
+
+# Hooks
+@hook('stop')
+def stop():
+    apply_playbook(
+        playbook='ansible/playbook.yaml',
+        tags=['uninstall'],
+        extra_vars={
+            'service_port': config.get('port')
+        }
+    )
+
+
+@hook('start')
+def start():
+    apply_playbook(
+        playbook='ansible/playbook.yaml',
+        tags=['install'],
+        extra_vars={
+            'service_port': config.get('port')
+        }
+    )
+    status_set('active', 'ready')
+
+
+@hook('upgrade-charm')
+def upgrade_charm():
+    remove_state('alerta.version')
+    remove_state('alerta.installed')
+    remove_state('alerta.configured')
+    status_set('active', 'ready')
